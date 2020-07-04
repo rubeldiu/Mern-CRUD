ad44b9837a987e9369ee9c7a7dde52359ea12385
Hamidul <rubeldiu@yahoo.com>
2020-07-04 13:38:44 +0200

    first commit

---
+++ server/controllers/post.js
@@ -0,0 +1,68 @@
+const Post = require('../models/post');
+const slugify = require('slugify');
+const post = require('../models/post');
+exports.create = (req, res) => {
+  // console.log(req.body);
+  const { title, content, user } = req.body;
+  const slug = slugify(title);
+  //validate
+  switch (true) {
+    case !title:
+      return res.status(400).json({ error: 'Title is required' });
+      break;
+    case !content:
+      return res.status(400).json({ error: 'Content is required' });
+      break;
+  }
+  /* res.json({
+    message: 'See your server console',
+  });*/
+  //create post
+  Post.create({ title, content, user, slug }, (err, post) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ error: 'Duplicate post. Try another title' });
+    }
+    res.json(post);
+  });
+};
+
+exports.list = (req, res) => {
+  Post.find({})
+    .limit(10)
+    .sort({ createdAt: -1 })
+    .exec((err, posts) => {
+      if (err) console.log(err);
+      res.json(posts);
+    });
+};
+
+exports.read = (req, res) => {
+  const { slug } = req.params;
+  Post.findOne({ slug }).exec((err, post) => {
+    if (err) console.log(err);
+    res.json(post);
+  });
+};
+
+exports.update = (req, res) => {
+  const { slug } = req.params;
+  const { title, content, user } = req.body;
+  Post.findOneAndUpdate({ slug }, { title, content, user }, { new: true }).exec(
+    (err, post) => {
+      if (err) console.log(err);
+      res.json(post);
+    }
+  );
+};
+
+exports.remove = (req, res) => {
+  const { slug } = req.params;
+
+  Post.findOneAndRemove({ slug }).exec((err, post) => {
+    if (err) console.log(err);
+    res.json({
+      message: 'Post deleted',
+    });
+  });
+};
---
+++ server/routers/post.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+//import controller methods
+const { create, list, read, update, remove } = require('../controllers/post');
+router.post('/post', create);
+router.get('/posts', list);
+router.get('/post/:slug', read); //read the single post
+router.put('/post/:slug', update);
+router.delete('/post/:slug', remove);
+module.exports = router;
